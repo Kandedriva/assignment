74f6ce16eab9a427884d7d98a7fa5a754a0b80a4
drissakande <114373066+Kandedriva@users.noreply.github.com>
2024-08-20 11:19:28 -0400

    Read file from db.json in the server.

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 import  express  from "express";
 import path from 'path';
 import { fileURLToPath } from "url";
-import cors from "cors"
+import cors from "cors";
+import fs from "fs";
 
 
 const app = express()
@@ -13,6 +14,30 @@ app.use(express.json());
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const dbPath = path.join(__dirname, 'db.json');
+
+
+function readDatabase() {
+    try {
+        const data = fs.readFileSync(dbPath, 'utf8');
+        console.log(data)
+        return JSON.parse(data);
+        
+    } catch (err) {
+        console.error('Error reading the database:', err);
+        return { employees: [], availablePositions: [] };
+    }
+}
+
+app.get("/", async(req, res)=>{
+    const db = await readDatabase();
+    res.json(db.employees)
+})
+
+
+
+
+
+
 
 app.listen(port, ()=>{
     console.log(`Server is up and running on port ${port}`)
