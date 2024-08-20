93c3bcf9d825ab8f1f073e726c6d305a1469e07a
drissakande <114373066+Kandedriva@users.noreply.github.com>
2024-08-20 10:47:30 -0400

    Add path in index.js.

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,18 @@
 import  express  from "express";
+import path from 'path';
+import { fileURLToPath } from "url";
+import cors from "cors"
 
 
 const app = express()
 const port = 5001;
+app.use(cors());
+app.use(express.json());
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const dbPath = path.join(__dirname, 'db.json');
 
 app.listen(port, ()=>{
     console.log(`Server is up and running on port ${port}`)
