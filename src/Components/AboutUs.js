c16758aa4ff2be2c52777e3dfe0ca3ed29e8c87a
drissakande <114373066+Kandedriva@users.noreply.github.com>
2024-08-20 14:26:47 -0400

    Create the AboutUs component.

---
+++ src/Components/AboutUs.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function AboutUs(){
+
+    return(
+        <div>
+            Lorem ipsum odor amet, consectetuer adipiscing elit. Rhoncus malesuada integer non nunc pharetra erat ut nullam. Magnis id a litora orci litora urna ligula vel. Sit orci ultrices varius aenean; fringilla aliquam. Adipiscing torquent curabitur commodo praesent pretium malesuada potenti hac. Dictum scelerisque per himenaeos habitasse pharetra lacus, platea sit felis. Sem leo nam penatibus; phasellus consectetur convallis potenti. Tempor condimentum viverra ipsum, purus donec dis. Scelerisque in rutrum convallis auctor taciti, et himenaeos montes. Pulvinar tellus purus eros vulputate nibh curabitur finibus convallis.
+        </div>
+    )
+}
+
+export default AboutUs
