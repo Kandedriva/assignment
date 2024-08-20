50f926e46d605cb7c3df1783067a8c9bac4b375b
drissakande <114373066+Kandedriva@users.noreply.github.com>
2024-08-20 12:07:33 -0400

    Fetch Employees inside the Emplfrom the server inside the employees component.

--- src/Components/Employees.js
+++ src/Components/Employees.js
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
 
 function Employees(){
+    const [employees, setEmployees] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:5001/employees")
+        .then(response =>response.json())
+        .then(employees =>setEmployees(employees))
+        
+    }, []);
+
     return(
         <>
             <h1>Hello Word..!</h1>
