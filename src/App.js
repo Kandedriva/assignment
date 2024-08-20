e132d53892ed9927cf279b9664b4525da6edd81b
drissakande <114373066+Kandedriva@users.noreply.github.com>
2024-08-20 14:57:27 -0400

    Create the Navbar component and the react to display the component separatyseparately.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import DomesticAnimals from './Components/DomesticAnimals';
 import WildAnimals from './Components/WildAnimals';
-// import DomesticAnimals from './Components/DomesticAnimals';
+import AboutUs from './Components/AboutUs';
+import Navbar from './Components/Navbar';
 
+// import DomesticAnimals from './Components/DomesticAnimals';
 
 
 function App() {
   return (
-    <div>
-    <DomesticAnimals/>
-    <WildAnimals/>
-    </div>
+    <Router>
+    <Navbar/>
+    <Routes>
+        <Route path="/" element={<AboutUs />} />
+        <Route path="/DomesticAnimals" element={<DomesticAnimals />} />
+        <Route path="/WildAnimals" element={<WildAnimals />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Components/Navbar.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+function Navbar(){
+
+    return(
+        <>
+        <div>
+            <li>
+                <Link to ="/">About Us</Link>
+            </li>
+            <li>
+                <Link to ="/WildAnimals">Wild Animals</Link>
+            </li>
+            <li>
+                <Link to ="/DomesticAnimals">Domestic Animals </Link>
+            </li>
+        </div>
+
+        </>
+    )
+}
+
+export default Navbar
